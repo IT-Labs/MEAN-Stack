eca47acd1d11fe031c4a194034aee0458778cbb4
Kole Vasilevski <kole.vasilevski@it-labs.com>
2020-04-14 19:54:47 +0200

    Initial commit

---
+++ src/server/mean-stack-server.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Server } from '@overnightjs/core';
+import { Logger } from '@overnightjs/logger';
+import TestController from './controllers/test-controller';
+
+class MeanStackServer extends Server {
+    
+    private readonly SERVER_START_MSG = 'Server started on port: ';
+    private readonly DEV_MSG = 'Express Server is running in development mode. ' +
+    'No front-end content is being served.';
+
+    constructor() {
+        super(true);
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+        super.addControllers(new TestController());
+
+        // Point to front-end code
+        if (process.env.NODE_ENV !== 'production') {
+            console.log('Starting server in development mode');
+            const msg = this.DEV_MSG + process.env.EXPRESS_PORT;
+            this.app.get('*', (req, res) => res.send(msg));
+        }
+    }
+
+    public start(port: number): void {
+        this.app.listen(port, () => {
+            Logger.Imp(this.SERVER_START_MSG + port);
+        });
+    }
+}
+
+export default MeanStackServer;
