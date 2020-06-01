6f88ebf4178d1ba2ac167a5d881f8f584ad9d916
Stefanija <stefanija.mihajlovska@it-labs.com>
2020-06-01 23:01:04 +0200

    tests for companies

--- src/server/tests/bank.spec.ts
+++ src/server/tests/bank.spec.ts
@@ -6,47 +6,25 @@ import { doesNotMatch } from 'assert';
 
 chai.use(chaiHttp);
 
-let server = new MeanStackServer();
+const server = new MeanStackServer();
 server.start(3001);
-let app = server.APPLICATION;
+const app = server.APPLICATION;
 
 chai.should();
-describe("Banks", () => {
-    describe("GET /", () => {
 
-        // Test to get all banks record
-        // it("should get all banks record", (done) => {
-        //      chai.request(app)
-        //          .get('/api/banks')
-        //          .then()
-        //          .end((err, res) => {
-        //              res.should.have.status(200);
-        //              res.body.should.be.a('Array');
-        //              done();
-        //           });
-        //  });        
-
-        //  it("should get all banks record", (done) => {
-        //     chai.request(app)
-        //         .get('/api/banks')
-        //         .then(res => {
-        //             res.should.have.status(200);
-        //             res.body.should.be.a('Array');
-        //          });
-        // });
-
-        it('should get all banks record', async () => {
-            const result = await chai.request(app).get('/api/banks');
-            result.should.have.status(200);
-            result.body.should.be.a('Array');
-        });
-
-        it('should get all banks record', async () => {
-            let id = "5eb060deb7450735fc26a004";
-            const result = await chai.request(app).get(`/api/banks/${id}`);
-            result.should.have.status(200);
-            result.body.should.be.a('object');
-        });
-         
+describe('Banks', () => {
+  describe('GET /', () => {
+    it('should get all banks record', async () => {
+      const result = await chai.request(app).get('/api/banks');
+      result.should.have.status(200);
+      result.body.should.be.a('Array');
     });
+
+    it('should get bank by specified id', async () => {
+      const id = '5eb060deb7450735fc26a004';
+      const result = await chai.request(app).get(`/api/banks/${id}`);
+      result.should.have.status(200);
+      result.body.should.be.a('object');
+    });
+  });
 });
