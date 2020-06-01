240e57d24370e709f42b65cec201a87ccac7fabf
Stefanija <stefanija.mihajlovska@it-labs.com>
2020-06-02 00:08:00 +0200

    add in memory db and mocha tests

--- src/server/services/client-service.ts
+++ src/server/services/client-service.ts
@@ -1,18 +1,34 @@
 import { MongoClient, Db } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-const uri: string = 'mongodb+srv://testuser:o98wHwKDlGeW7QaK@testcluster-qhjws.mongodb.net/test?retryWrites=true&w=majority';
+const mongod = new MongoMemoryServer();
+
+const uri: string =
+  'mongodb+srv://testuser:o98wHwKDlGeW7QaK@testcluster-qhjws.mongodb.net/test?retryWrites=true&w=majority';
+
 let db: Db;
 let client: MongoClient;
 
 export async function connectToMongoServer() {
+
+  if(process.env.NODE_ENV === 'test'){
+    const testUri = await mongod.getConnectionString('mean_stack');
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+      };
+    client = new MongoClient(testUri, mongooseOpts);
+  }else{
     const config = { useUnifiedTopology: true, useNewUrlParser: true };
-    client = new MongoClient(uri, config );
-    await client.connect();
-    //db = client.db(process.env.db);
-    db = client.db("mean_stack")
+    client = new MongoClient(uri, config);
+  }
+
+  await client.connect();
+  db = client.db('mean_stack');
 }
 
 export function getDb(): Db {
-    return db;
+  return db;
 }
-
