160480398feee559c698f218634de6a26a6358b1
koleVasilevski1969 <kole.vasilevski@it-labs.com>
2020-07-23 09:09:46 +0200

    Initial Angular infrastructure

--- src/server/public/angular/client-app/src/app/app-routing.module.ts
+++ src/server/public/angular/client-app/src/app/app-routing.module.ts
@@ -1,8 +1,53 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { CompanyComponent } from './components/company/company.component';
+import { LoginComponent } from './components/login/login.component';
+import { SecureLayoutComponent } from './layouts/secure-layout/secure-layout.component';
 
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: LoginComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    data: { title: "Login" },
+  },
+  {
+    path: "",
+    component: SecureLayoutComponent,
+    children: [
+      {
+        path: "companies",
+        component: CompaniesComponent,
+        data: {
+          title: "Companies",
+        },
+      },
+      {
+        path: "companies/:id",
+        component: CompanyComponent,
+        data: {
+          title: "company",
+        },
+      },
+      {
+        path: "companies/new",
+        component: CompanyComponent,
+        data: {
+          title: "company",
+        },
+      }
+    ]
+  }
+  
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/server/public/angular/client-app/src/app/app.module.ts
+++ src/server/public/angular/client-app/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SecureLayoutComponent } from './layouts/secure-layout/secure-layout.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MainComponent } from './layouts/main/main.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CompaniesComponent,
+    CompanyComponent,
+    SecureLayoutComponent,
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
