8547d29c4b17455b3c8a5888493824af87f02d3f
koleVasilevski1969 <kole.vasilevski@it-labs.com>
2020-10-24 16:37:12 +0200

    Test improvements

--- src/server/tests/bank.spec.ts
+++ src/server/tests/bank.spec.ts
@@ -14,39 +14,37 @@ chai.should();
 describe("Banks", () => {
     describe("GET /", () => {
 
-        // Test to get all banks record
-        // it("should get all banks record", (done) => {
-        //      chai.request(app)
-        //          .get('/api/banks')
-        //          .then()
-        //          .end((err, res) => {
-        //              res.should.have.status(200);
-        //              res.body.should.be.a('Array');
-        //              done();
-        //           });
-        //  });        
-
-        //  it("should get all banks record", (done) => {
-        //     chai.request(app)
-        //         .get('/api/banks')
-        //         .then(res => {
-        //             res.should.have.status(200);
-        //             res.body.should.be.a('Array');
-        //          });
-        // });
-
         it('should get all banks record', async () => {
             const result = await chai.request(app).get('/api/banks');
             result.should.have.status(200);
             result.body.should.be.a('Array');
-        });
+        }).timeout(5000);
 
-        it('should get all banks record', async () => {
+        it('should get one banks record', async () => {
             let id = "5eb060deb7450735fc26a004";
             const result = await chai.request(app).get(`/api/banks/${id}`);
             result.should.have.status(200);
             result.body.should.be.a('object');
-        });
+        }).timeout(5000);;
+         
+    });
+});
+
+describe("Companies", () => {
+    describe("GET /", () => {
+
+        it('should get all companies record', async () => {
+            const result = await chai.request(app).get('/api/companies');
+            result.should.have.status(200);
+            result.body.should.be.a('Array');
+        }).timeout(5000);
+
+        it('should get one company record', async () => {
+            let id = "5eb14c90f3640e73abb43003";
+            const result = await chai.request(app).get(`/api/company/${id}`);
+            result.should.have.status(200);
+            result.body.should.be.a('object');
+        }).timeout(5000);;
          
     });
 });
