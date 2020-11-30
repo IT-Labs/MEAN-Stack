a7d0f546956ea82af3ba7855c43b58768b9e41d8
Nikola Kuzmanovski <nikola.kuzmanovski@it-labs.com>
2020-11-30 19:41:07 +0100

    companies layout change / add icons

--- src/server/public/angular/client-app/src/app/components/components.module.ts
+++ src/server/public/angular/client-app/src/app/components/components.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-// import { ScriptLoader } from '../shared/core/loader/services/script-loader.service';
-// import { SvgIconsModule } from '../svg-icons/svg-icons.module';
-// import { SvgIconRegistry } from '../svg-icons/svg-icon-registry.service';
-import { iconAvatar } from '../../assets/svg/svg-icons';
+import { SvgIconsModule } from '@ngneat/svg-icon';
 import { LoginComponent } from './login/login.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { CompanyComponent } from './company/company.component';
@@ -13,6 +10,8 @@ import { BanksComponent } from './banks/banks.component';
 import { BankComponent } from './bank/bank.component';
 import { CompanyAccountsComponent } from './company-accounts/company-accounts.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KitchenSinkModule } from '../kitchen-sink/kitchen-sink.module';
+
 const DECLARATIONS = [
   LoginComponent,
   CompaniesComponent,
@@ -28,14 +27,8 @@ const MODULES = [
   BrowserModule,
   HttpClientModule,
   FormsModule,
-  //   SvgIconsModule.forRoot({
-  //     sizes: {
-  //       xs: '10px',
-  //       sm: '12px',
-  //       md: '16px',
-  //       lg: '20px',
-  //     },
-  //   }),
+  SvgIconsModule.forRoot(),
+  KitchenSinkModule,
 ];
 
 const PROVIDERS = [];
@@ -43,12 +36,7 @@ const PROVIDERS = [];
 @NgModule({
   declarations: [...DECLARATIONS],
   imports: [...MODULES],
-  exports: [...DECLARATIONS, ...MODULES],
+  exports: [...DECLARATIONS],
   providers: [...PROVIDERS],
 })
-export class ComponentsModule {
-  // constructor(private scriptLoader: ScriptLoader, private svgIconRegistry: SvgIconRegistry) {
-  //   this.scriptLoader.intlTelInput();
-  //   this.svgIconRegistry.register(iconAvatar);
-  // }
-}
+export class ComponentsModule {}
